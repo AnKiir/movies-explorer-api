c56206901c883429d48511b3a191f2e5ddc7472c
AzKir <a.kirykhina@yandex.ru>
2023-08-05 21:17:27 +0300

    Models
    Добавлены схемы для фильмов и для пользователя.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express');
+// const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('cors');
+require('dotenv').config();
+
+const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+app.use(requestLogger);
+app.use(errorLogger);
+app.use(errors());
+
+app.listen(PORT);
