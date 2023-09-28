445fda1b1df3fde53207da409f0f6edcb1ab8187
AzKir <a.kirykhina@yandex.ru>
2023-09-28 20:29:39 +0300

    Update validators.js
    убран закоменченный код

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -39,12 +39,6 @@ const postMovieCelebrate = celebrate({
   }),
 });
 
-// const deleteMovieCelebrate = celebrate({
-//   params: Joi.object().keys({
-//     movieId: Joi.number().required(),
-//   }),
-// });
-
 const deleteMovieCelebrate = celebrate({
   params: Joi.object().keys({
     movieId: Joi.string().length(24).hex().required(),
